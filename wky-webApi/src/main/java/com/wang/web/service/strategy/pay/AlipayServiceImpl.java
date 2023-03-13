d3ea11b97a22ee51fa71ca94c3bae9e32bd93f72
wkyou <wangkanyou@1fda.com>
2023-03-13 09:24:25 +0800

    测试自动部署更新8

--- wky-webApi/src/main/java/com/wang/web/service/strategy/pay/AlipayServiceImpl.java
+++ wky-webApi/src/main/java/com/wang/web/service/strategy/pay/AlipayServiceImpl.java
@@ -19,7 +19,7 @@ public class AlipayServiceImpl implements PayService{
 
     @Override
     public PayResult unifiedOrder(PayEntity payEntity) {
-        String msg="这里是支付宝下单支付接口，你支付的金额为："+payEntity.getPayAmount()+"---zfb7";
+        String msg="这里是支付宝下单支付接口，你支付的金额为："+payEntity.getPayAmount()+"---zfb8";
         return PayResult.Build.success(msg);
     }
 }
